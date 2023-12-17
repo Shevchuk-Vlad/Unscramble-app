c334faccef990f82b150d6f1b864af39e29d4190
Vlad <Vlad17.lutsk@gmail.com>
2023-12-17 15:49:28 +0200

    Move data to the ViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,4 +4,15 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
+    private var _count = 0
+    val count: Int
+        get() = _count
+
+
+    private var currentWordCount = 0
+
+
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
 }
